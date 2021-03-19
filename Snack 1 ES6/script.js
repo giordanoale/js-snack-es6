d8ddca75606c6fdccf274c683c4148fe0adaa4a3
giordanoale <a.giordano89@gmail.com>
2021-03-19 15:55:24 +0100

    Risolto Snack 1

--- Snack 1 ES6/script.js
+++ Snack 1 ES6/script.js
@@ -1,3 +1,31 @@
 // Creare un array di oggetti:
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+const biciclette = [
+    {
+        nome: "bianchi",
+        peso: 1600,
+    },
+    {
+        nome: "atala",
+        peso: 2000,
+    },
+    {
+        nome: "vangoof",
+        peso: 1300,
+    },
+]
+
+console.log(biciclette);
+
+var biciLeggera = biciclette[0].peso;
+
+for (var i = 0; i < biciclette.length; i++) {
+
+    if (biciclette[i].peso < biciLeggera) {
+        biciLeggera = biciclette[i];
+    }
+}
+
+console.log(` La bici più leggera è la ${biciLeggera.nome} con un peso di ${biciLeggera.peso} grammi`);
